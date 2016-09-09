900ebfdb0c7154e0b1b02e8a2efc085705e0f93b
rubenabix <rubenabix@egmail.com>
2016-09-09 17:35:19 -0600

    update juui app

--- src/server/lib/web/juui/routes.js
+++ src/server/lib/web/juui/routes.js
@@ -1,21 +1,21 @@
 'use strict';
 
 const routes = [
-  {
-    method: 'GET',
-    path: '/juui/',
-    config: {
-      cache: false,
-      handler(request, reply){
-
-        reply.view('./juui/app/templates/index.pug',
-          {});
-
-      },
-      tags: ['web'],
-      validate: {}
-    }
-  }
+  // {
+  //   method: 'GET',
+  //   path: '/juui/',
+  //   config: {
+  //     cache: false,
+  //     handler(request, reply){
+  //
+  //       reply.view('./juui/app/templates/index.html',
+  //         {});
+  //
+  //     },
+  //     tags: ['web'],
+  //     validate: {}
+  //   }
+  // }
 ];
 
 module.exports = routes;
