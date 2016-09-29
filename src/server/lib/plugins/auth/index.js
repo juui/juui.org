631c3e9b744725611633ab05ee704b732eb9f664
rubenabix <rubenabix@egmail.com>
2016-09-29 10:33:41 -0600

    update auth

--- src/server/lib/plugins/auth/index.js
+++ src/server/lib/plugins/auth/index.js
@@ -9,7 +9,7 @@ exports.register = function (server, options, next) {
   server.auth.strategy('facebook', 'bell',
     {
       provider: 'facebook',
-      password: process.env.SECRET_FACEBOOK || 'facebook',
+      password: process.env.SECRET_FACEBOOK || 'B182393C267BB36131E0A1B90252573677C72D42D25E770701DBC077228DDF3A',
       clientId: '203280530023571', // fill in your FB ClientId here
       clientSecret: '238b5c7b374c2537081c0cd301b24548', // fill in your FB Client Secret here
       isSecure: true, // Terrible idea but required if not using HTTPS
@@ -21,7 +21,7 @@ exports.register = function (server, options, next) {
   server.auth.strategy('google', 'bell',
     {
       provider: 'google',
-      password: process.env.SECRET_GOOGLE || 'google',
+      password: process.env.SECRET_GOOGLE || 'B182393C267BB36131E0A1B90252573677C72D42D25E770701DBC077228DDF3A',
       clientId: '795777413258-5aal4m6ndu98huaoo2gn82q0hu62va7q.apps.googleusercontent.com', // fill in your Google ClientId here
       clientSecret: 'bTmKsc5PbVaUbN2AYOzz1yJi', // fill in your Google Client Secret here
       isSecure: true, // Terrible idea but required if not using HTTPS,
@@ -33,7 +33,7 @@ exports.register = function (server, options, next) {
   server.auth.strategy('twitter', 'bell',
     {
       provider: 'twitter',
-      password: process.env.SECRET_TWITTER || 'twitter',
+      password: process.env.SECRET_TWITTER || 'B182393C267BB36131E0A1B90252573677C72D42D25E770701DBC077228DDF3A',
       clientId: 'z7CzhqNUaMlw96UrNYH0ARqLO',
       clientSecret: 'q8QoEfj2HIdrDrUskUJV1Hw4FvpP7UObuoOhLLvEBwY3HxzFeO',
       isSecure: true,
@@ -43,7 +43,7 @@ exports.register = function (server, options, next) {
   );
 
   server.auth.strategy('session', 'cookie', {
-    password: process.env.SECRET_COOKIES || 'cookie', // give any string you think is right password to encrypted
+    password: process.env.SECRET_COOKIES || 'B182393C267BB36131E0A1B90252573677C72D42D25E770701DBC077228DDF3A', // give any string you think is right password to encrypted
     cookie: 'sid-juui', // cookie name to use, usually sid-<appname>
     redirectTo: '/',
     isSecure: true,
